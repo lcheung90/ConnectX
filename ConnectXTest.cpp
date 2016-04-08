623b9c2d5e5090bd4aa04cd006fb2e927ea82fa6
lcheung <lcheung90@gmail.com>
2016-04-08 01:34:04 -0700

    [Feature] Add unit tests

--- ConnectXTest.cpp
+++ ConnectXTest.cpp
@@ -5,7 +5,7 @@
 #include <gtest/gtest.h>
 #include <iostream>
 #include "ConnectX.h"
- 
+
 class ConnectXTest : public ::testing::Test
 {
 	protected:
@@ -18,4 +18,102 @@ class ConnectXTest : public ::testing::Test
 TEST(ConnectXTest, sanityCheck)
 {
 	ASSERT_TRUE(true);
+}
+
+TEST(ConnectXTest, outBoundsOverTest)
+{
+  ConnectX c = ConnectX(1,1,1);
+  c.placePiece(10);
+  ASSERT_TRUE( c.at(0,0) == 0 );
+}
+
+TEST(ConnectXTest, outBoundsUnderTest)
+{
+  ConnectX c = ConnectX(1,1,1);
+  c.placePiece(-10);
+  ASSERT_TRUE( c.at(0,0) == 0 );
+}
+
+TEST(ConnectXTest, outBoundsAtPosXTest)
+{
+  ConnectX c = ConnectX(1,1,1);
+  c.placePiece(0);
+  ASSERT_TRUE( c.at(10,0) == -1 ); 
+}
+
+TEST(ConnectXTest, outBoundsAtNegXTest)
+{
+  ConnectX c = ConnectX(1,1,1);
+  c.placePiece(0);
+  ASSERT_TRUE( c.at(-10,0) == -1 );
+}
+
+TEST(ConnectXTest, outBoundsAtPosYTest)
+{
+  ConnectX c = ConnectX(1,1,1);
+  c.placePiece(0);
+  ASSERT_TRUE( c.at(0,10) == -1 );
+}
+
+TEST(ConnectXTest, outBoundsAtNegYTest)
+{
+  ConnectX c = ConnectX(1,1,1);
+  c.placePiece(0);
+  ASSERT_TRUE( c.at(0,-10) == -1 );
+}
+
+TEST(ConnectXTest, constructorDefaultDimsWidthTest)
+{
+  ConnectX c = ConnectX(0,5,1);
+  ASSERT_TRUE( c.at(DEFAULT_WIDTH-1,DEFAULT_HEIGHT-1) == 0 );
+  ASSERT_TRUE( c.at(0,DEFAULT_HEIGHT-1) == 0 );
+  ASSERT_TRUE( c.at(DEFAULT_WIDTH-1,0) == 0 );
+  ASSERT_TRUE( c.at(0,0) == 0 );
+}
+
+TEST(ConnectXTest, constructorDefaultDimsHeightTest)
+{
+  ConnectX c = ConnectX(5,0,1);
+  ASSERT_TRUE( c.at(DEFAULT_WIDTH-1,DEFAULT_HEIGHT-1) == 0 );
+  ASSERT_TRUE( c.at(0,DEFAULT_HEIGHT-1) == 0 );
+  ASSERT_TRUE( c.at(DEFAULT_WIDTH-1,0) == 0 );
+  ASSERT_TRUE( c.at(0,0) == 0 );
+}
+
+TEST(ConnectXTest, constructorDefaultToWinTest)
+{
+  ConnectX c = ConnectX(5,6,0);
+  ASSERT_TRUE( c.at(DEFAULT_WIDTH-1,DEFAULT_HEIGHT-1) == 0 );
+  ASSERT_TRUE( c.at(0,DEFAULT_HEIGHT-1) == 0 );
+  ASSERT_TRUE( c.at(DEFAULT_WIDTH-1,0) == 0 );
+  ASSERT_TRUE( c.at(0,0) == 0 );
+}
+
+TEST(ConnectXTest, occupiedTest)
+{
+  ConnectX c = ConnectX(1,1,1);
+  c.placePiece(0);
+  c.placePiece(0);
+  ASSERT_TRUE( c.at(0,0) == 2); //black
+}
+
+TEST(ConnectXTest, missTurnTest)
+{
+  ConnectX c = ConnectX(1,1,1);
+  c.placePiece(-10);
+  c.placePiece(0);
+  ASSERT_TRUE( c.at(0,0) == 1); //white
+}
+
+TEST(ConnectXTest, initTurnCheckTest)
+{
+  ConnectX c = ConnectX(1,1,1);
+  ASSERT_TRUE( c.whoseTurn() == 2 );
+}
+
+TEST(ConnectXTest, toggleTurnCheckTest)
+{
+  ConnectX c = ConnectX(1,1,1);
+  c.placePiece(0);
+  ASSERT_TRUE( c.whoseTurn() == 1 );
 }
